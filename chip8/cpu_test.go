23827d77c6518d37a1fe6f924488c523cd917b92
Simon Juba <scimail09@gmail.com>
2022-06-02 02:53:51 +0900

    feat: add missing opcodes

--- chip8/cpu_test.go
+++ chip8/cpu_test.go
@@ -1,14 +1,13 @@
 package chip8
 
 import (
-	"log"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestFetch(t *testing.T) {
-	machine := Machine{
+func TestCPU(t *testing.T) {
+	machine := CPU{
 		Memory: make([]byte, 4096),
 		PC:     0x200,
 	}
@@ -47,29 +46,5 @@ func TestFetch(t *testing.T) {
 
 			assert.Equal(t, uint16(tt.args.Want), code, "Instruction should be loaded")
 		})
-	}
-}
-
-func TestStack(t *testing.T) {
-	machine := Machine{
-		Stack: NewStack(),
-	}
-	for _, val := range machine.Stack.addr {
-		assert.Zero(t, val, "Stack should start empty")
-	}
-
-	instruction := Address(0xFFCC)
-	machine.Stack.Push(instruction)
-	assert.Contains(t, machine.Stack.addr, instruction, "stack should contain added instruction")
-
-	assert.Equal(t, len(machine.Stack.addr), 1)
-
-	log.Println(machine.Stack)
-	got := machine.Stack.Pop()
-	assert.Equal(t, got, instruction, "should return added item")
-
-	log.Println(machine.Stack)
-	for _, val := range machine.Stack.addr {
-		assert.Zero(t, val, "Stack should be now empty")
 	}
 }
